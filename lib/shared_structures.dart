c98e6416650f3c6b56c73bbda557122291e1d3d5
Emil Persson <emil.n.persson@gmail.com>
2015-07-03 16:10:46 +0200

    Clean

--- lib/shared_structures.dart
+++
@@ -1,6 +0,0 @@
-import 'package:bridge/src/tether/library_shared.dart';
-import 'package:bridge/src/view/shared/library.dart';
-
-registerSharedStructures(Tether tether) {
-  tether.registerStructure('Template', Template, (s) => new Template.deserialize(s));
-}
