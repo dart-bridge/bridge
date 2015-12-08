0760009b69fe8efeca32eefcd2ad2a3399460ccc
Emil Persson <emil.n.persson@gmail.com>
2015-12-08 11:26:59 +0100

    Converting to Response objects in Error Handlers no longer needed

--- lib/main/error_controller.dart
+++ lib/main/error_controller.dart
@@ -6,39 +6,20 @@ class ErrorController {
   // lib/templates/error.chalk.html
   static const errorTemplate = 'error';
 
-  Future<Response> general(
-      Object exception,
-      StackTrace stack) {
+  general(Object exception, StackTrace stack) {
     print('<red>Uncaught error!\n$exception</red>');
-    return _errorTemplate(exception, stack);
-  }
 
-  Future<Response> notFound(
-      HttpNotFoundException exception,
-      StackTrace stack) {
-    return _errorTemplate(exception, stack, 404, {
-      'message': "Oops! That page wasn't found!"
-    });
-  }
-
-  Future<Response> _errorTemplate(
-      Object exception,
-      StackTrace stack,
-      [int code = 500,
-      Map<String, dynamic> variables = const {}]) async {
-
-    // Create a template with the specified parameters
-    final errorTemplate = await (template(errorTemplate, withData: variables)
+    return template(errorTemplate)
       ..exception = exception
       ..stackTrace = stack
-      ..code = code);
+      ..code = 500;
+  }
 
-    // Return a shelf Response from the template and the params
-    return new Response(
-        code,
-        body: errorTemplate.encoded,
-        headers: {
-          'Content-Type': ContentType.HTML.toString()
-        });
+  notFound(HttpNotFoundException exception, StackTrace stack) {
+    return template(errorTemplate)
+      ..message = "Oops! That page wasn't found!"
+      ..exception = exception
+      ..stackTrace = stack
+      ..code = 404;
   }
 }
